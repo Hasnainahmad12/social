a9d4346d238d20b58257cfff243c6c9ccbbeee37
Hasnain Ahmad <hasnainbangash03@gmail.com>
2022-12-26 16:47:12 +0500

    first commit

---
+++ src/pages/MovieScreen.js
@@ -0,0 +1,410 @@
+import "antd/dist/antd.min.css";
+import { Button } from "@mui/material";
+import { Button as AntButton } from "antd";
+import "./MovieScreen.css";
+
+const MovieScreen = () => {
+  return (
+    <div className="movie-screen">
+      <div className="movie-screen-child" />
+      <img className="movie-screen-item" alt="" src="../rectangle-14@2x.png" />
+      <img className="movie-screen-inner" alt="" src="../rectangle-14@2x.png" />
+      <img className="rectangle-icon" alt="" src="../rectangle-12@2x.png" />
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="galberg-324-best-drama">Galberg 324 , best drama</div>
+        <div className="dec-2021">12 Dec 2021</div>
+        <img className="group-inner" alt="" src="../vector-5.svg" />
+      </div>
+      <div className="rectangle-group">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="cinema-center">Galberg 324 , best drama</div>
+        <div className="dec-20211">12 Dec 2021</div>
+        <div className="wrapper">
+          <div className="div">02 : 12 : 29</div>
+        </div>
+        <img className="group-inner" alt="" src="../vector-51.svg" />
+      </div>
+      <div className="rectangle-container">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="cinema-center">Galberg 324 , best drama</div>
+        <div className="dec-20211">12 Dec 2021</div>
+        <div className="wrapper">
+          <div className="div">02 : 12 : 29</div>
+        </div>
+        <img className="group-inner" alt="" src="../vector-52.svg" />
+      </div>
+      <div className="movie-screen-child1" />
+      <img className="movie-screen-child2" alt="" src="../rectangle-9@2x.png" />
+      <img
+        className="movie-screen-child3"
+        alt=""
+        src="../rectangle-15@2x.png"
+      />
+      <img
+        className="movie-screen-child4"
+        alt=""
+        src="../rectangle-15@2x.png"
+      />
+      <button className="rectangle-button" />
+      <div className="movie-screen-child5" />
+      <div className="movie-screen-child6" />
+      <img
+        className="movie-screen-child7"
+        alt=""
+        src="../rectangle-10@2x.png"
+      />
+      <img
+        className="movie-screen-child8"
+        alt=""
+        src="../rectangle-17@2x.png"
+      />
+      <img
+        className="movie-screen-child9"
+        alt=""
+        src="../rectangle-17@2x.png"
+      />
+      <div className="movie-screen-child10" />
+      <img className="movie-screen-child11" alt="" src="../vector-1.svg" />
+      <div className="ellipse-parent">
+        <img className="ellipse-icon" alt="" src="../ellipse-1@2x.png" />
+        <div className="infogmailcom-parent">
+          <div className="infogmailcom">info@gmail.com</div>
+          <div className="alexa">Alexa</div>
+          <img className="arrow-down-2-icon" alt="" src="../arrowdown2.svg" />
+        </div>
+      </div>
+      <div className="ellipse-group">
+        <img className="group-child4" alt="" src="../ellipse-2.svg" />
+        <div className="iconlyboldnotification-parent">
+          <img
+            className="iconlyboldnotification"
+            alt=""
+            src="../iconlyboldnotification.svg"
+          />
+          <img className="group-child5" alt="" src="../ellipse-21.svg" />
+          <div className="div2">1</div>
+        </div>
+      </div>
+      <div className="ellipse-container">
+        <img className="group-child4" alt="" src="../ellipse-2.svg" />
+        <div className="iconlyboldmessage-parent">
+          <img
+            className="iconlyboldmessage"
+            alt=""
+            src="../iconlyboldmessage.svg"
+          />
+          <img className="group-child7" alt="" src="../ellipse-31.svg" />
+          <div className="div3">1</div>
+        </div>
+      </div>
+      <h2 className="recommended" id="rcom" recom="Recommendation">
+        Recommended
+      </h2>
+      <b className="popular-movies">Popular Movies</b>
+      <b className="new-movies">New Movies</b>
+      <div className="movie-screen-child12" />
+      <div className="movie-screen-child13" />
+      <img className="search-icon" alt="" src="../search.svg" />
+      <img className="iconlyboldfilter" alt="" src="../iconlyboldfilter.svg" />
+      <div className="search-movie-here">Search movie here</div>
+      <div className="genre">Genre</div>
+      <div className="group-parent">
+        <div className="group-div">
+          <div className="group-child8" />
+          <div className="action">Action</div>
+          <img className="vector-icon1" alt="" src="../vector.svg" />
+        </div>
+        <div className="rectangle-parent1">
+          <div className="group-child9" />
+          <div className="comedy">Comedy</div>
+          <div className="country">Country</div>
+          <img className="vector-icon2" alt="" src="../vector1.svg" />
+        </div>
+      </div>
+      <div className="group-container">
+        <div className="rectangle-parent2">
+          <div className="group-child10" />
+          <div className="action">Drama</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+        <div className="rectangle-parent3">
+          <div className="group-child10" />
+          <div className="action">Horror</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+      </div>
+      <div className="group-parent1">
+        <div className="rectangle-parent2">
+          <div className="group-child10" />
+          <div className="action">Mythological</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+        <div className="rectangle-parent3">
+          <div className="group-child10" />
+          <div className="action">War</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+      </div>
+      <div className="group-parent2">
+        <div className="rectangle-parent2">
+          <div className="group-child10" />
+          <div className="action">Adventure</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+        <div className="rectangle-parent3">
+          <div className="group-child10" />
+          <div className="action">Family</div>
+          <img className="vector-icon3" alt="" src="../vector1.svg" />
+        </div>
+      </div>
+      <img
+        className="movie-screen-child14"
+        alt="img"
+        src="../rectangle-8@2x.png"
+      />
+      <h1 className="the-crazy-life" id="img__head">
+        THE CRAZY LIFE
+      </h1>
+      <img className="movie-screen-child15" alt="" src="../ellipse-2.svg" />
+      <img className="home-icon" alt="" src="../home.svg" />
+      <Button
+        className="group-button"
+        sx={{ width: 175 }}
+        variant="contained"
+        color="primary"
+      >
+        Watch Now
+      </Button>
+      <img className="movie-screen-child16" alt="" src="../group-27.svg" />
+      <img className="movie-screen-child17" alt="" src="../group-27.svg" />
+      <img className="movie-screen-child18" alt="" src="../group-27.svg" />
+      <div className="toy-story-land">Toy Story land</div>
+      <div className="madmax-dad-the-crazy">Madmax Dad , The Crazy</div>
+      <div className="madmax-dad-the-crazy1">Madmax Dad , The Crazy</div>
+      <div className="dec-20213">12 Dec 2021</div>
+      <div className="dec-20214">12 Dec 2021</div>
+      <div className="dec-20215">12 Dec 2021</div>
+      <div className="frame">
+        <div className="div">02 : 12 : 29</div>
+      </div>
+      <div className="frame-div">
+        <div className="div">02 : 12 : 29</div>
+      </div>
+      <div className="wrapper1">
+        <div className="div">02 : 12 : 29</div>
+      </div>
+      <img className="movie-screen-child19" alt="" src="../vector-3.svg" />
+      <img className="movie-screen-child20" alt="" src="../vector-53.svg" />
+      <img className="movie-screen-child21" alt="" src="../vector-12.svg" />
+      <div className="rectangle-parent8">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="cinema-center">Cinema Center</div>
+        <div className="dec-20211">12 Dec 2021</div>
+        <div className="wrapper">
+          <div className="div">02 : 12 : 29</div>
+        </div>
+        <img className="group-inner" alt="" src="../vector-54.svg" />
+      </div>
+      <div className="rectangle-parent9">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="cinema-center">Cinema Center</div>
+        <div className="dec-20211">12 Dec 2021</div>
+        <div className="wrapper">
+          <div className="div">02 : 12 : 29</div>
+        </div>
+        <img className="group-inner" alt="" src="../vector-55.svg" />
+      </div>
+      <div className="rectangle-parent10">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="../group-27.svg" />
+        <div className="cinema-center">Cinema Center</div>
+        <div className="dec-20211">12 Dec 2021</div>
+        <div className="wrapper">
+          <div className="div">02 : 12 : 29</div>
+        </div>
+        <img className="group-inner" alt="" src="../vector-56.svg" />
+      </div>
+      <img className="movie-screen-child22" alt="" src="../vector-4.svg" />
+      <img className="movie-screen-child23" alt="" src="../vector-6.svg" />
+      <img className="movie-screen-child24" alt="" src="../vector-13.svg" />
+      <AntButton
+        className="group-antbutton"
+        style={{ width: "92px" }}
+        type="default"
+        size="middle"
+        shape="default"
+      >
+        Trending
+      </AntButton>
+      <div className="rectangle-parent11">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector8.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent12">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector9.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent13">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector10.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent14">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector11.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent15">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector12.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent16">
+        <div className="group-child25" />
+        <div className="trending-parent">
+          <div className="trending">Trending</div>
+          <img className="vector-icon9" alt="" src="../vector13.svg" />
+        </div>
+      </div>
+      <div className="movies">12 Movies</div>
+      <div className="movies1">32 Movies</div>
+      <div className="movies2">24 Movies</div>
+      <div className="arrow-down-2-parent">
+        <img className="arrow-down-2-icon1" alt="" src="../arrowdown21.svg" />
+        <div className="see-all">See All</div>
+      </div>
+      <div className="arrow-down-2-group">
+        <img className="arrow-down-2-icon1" alt="" src="../arrowdown21.svg" />
+        <div className="see-all">See All</div>
+      </div>
+      <div className="arrow-down-2-container">
+        <img className="arrow-down-2-icon1" alt="" src="../arrowdown21.svg" />
+        <div className="see-all">See All</div>
+      </div>
+      <img className="movie-screen-child25" alt="" src="../vector-7.svg" />
+      <div className="rectangle-parent17">
+        <div className="group-child31" />
+        <div className="book-marked">Book Marked</div>
+        <img
+          className="vuesaxlineararchive-add-icon"
+          alt=""
+          src="../vuesaxlineararchiveadd.svg"
+        />
+      </div>
+      <img
+        className="movie-screen-child26"
+        alt=""
+        src="../rectangle-91@2x.png"
+      />
+      <img className="movie-screen-child27" alt="" src="../group-211.svg" />
+      <div className="hollywood-tower">Hollywood Tower</div>
+      <div className="dec-20219">12 Dec 2021</div>
+      <div className="div10">12:32</div>
+      <div className="div11">01:12:55</div>
+      <img className="movie-screen-child28" alt="" src="../vector-8.svg" />
+      <img className="movie-screen-child29" alt="" src="../vector-9.svg" />
+      <img className="movie-screen-child30" alt="" src="../ellipse-11.svg" />
+      <img className="vector-icon15" alt="" src="../vector.svg" />
+      <div className="rectangle-parent18">
+        <img className="group-child32" alt="" src="../rectangle-18@2x.png" />
+        <img className="group-child33" alt="" src="../group-211.svg" />
+        <div className="hollywood-tower1">Hollywood Tower</div>
+        <div className="dec-202110">12 Dec 2021</div>
+        <div className="div12">12:32</div>
+        <div className="div13">01:12:55</div>
+        <img className="group-child34" alt="" src="../vector-8.svg" />
+        <img className="group-child35" alt="" src="../vector-9.svg" />
+        <img className="group-child36" alt="" src="../ellipse-11.svg" />
+        <img className="vector-icon16" alt="" src="../vector.svg" />
+      </div>
+      <div className="rectangle-parent19">
+        <img className="group-child32" alt="" src="../rectangle-181@2x.png" />
+        <img className="group-child33" alt="" src="../group-211.svg" />
+        <div className="hollywood-tower1">Hollywood Tower</div>
+        <div className="dec-202110">12 Dec 2021</div>
+        <div className="div12">12:32</div>
+        <div className="div13">01:12:55</div>
+        <img className="group-child34" alt="" src="../vector-8.svg" />
+        <img className="group-child35" alt="" src="../vector-9.svg" />
+        <img className="group-child36" alt="" src="../ellipse-11.svg" />
+        <img className="vector-icon16" alt="" src="../vector.svg" />
+      </div>
+      <div className="rectangle-parent20">
+        <div className="group-child42" />
+        <div className="new">New</div>
+      </div>
+      <img
+        className="vuesaxlineararchive-add-icon1"
+        alt=""
+        src="../vuesaxlineararchiveadd1.svg"
+      />
+      <img className="arrow-down-2-icon4" alt="" src="../arrowdown2.svg" />
+      <img className="movie-screen-child31" alt="" src="../vector-14.svg" />
+      <div className="rectangle-parent21">
+        <div className="group-child43" />
+        <img className="group-child44" alt="" src="../vector-57.svg" />
+        <div className="united-states">United States</div>
+        <div className="china">China</div>
+        <div className="vector-parent">
+          <img className="group-child45" alt="" src="../vector-61.svg" />
+          <div className="india">India</div>
+        </div>
+        <div className="vector-group">
+          <img className="group-child45" alt="" src="../vector-62.svg" />
+          <div className="iran">Iran</div>
+        </div>
+        <div className="vector-container">
+          <img className="group-child45" alt="" src="../vector-63.svg" />
+          <div className="japan">Japan</div>
+        </div>
+        <div className="vector-parent1">
+          <img className="group-child45" alt="" src="../vector-64.svg" />
+          <div className="turkey">Turkey</div>
+        </div>
+        <div className="vector-parent2">
+          <img className="group-child45" alt="" src="../vector-65.svg" />
+          <div className="japan">Russia</div>
+        </div>
+        <div className="vector-parent3">
+          <img className="group-child45" alt="" src="../vector-66.svg" />
+          <div className="turkey">France</div>
+        </div>
+        <div className="vector-parent4">
+          <img className="group-child51" alt="" src="../vector-67.svg" />
+          <div className="united-kingdom">United Kingdom</div>
+        </div>
+      </div>
+      <div className="movies-parent">
+        <b className="movies3">Movies</b>
+        <div className="series-wrapper">
+          <div className="series">Series</div>
+        </div>
+        <div className="see-all">All</div>
+        <div className="group-child52" />
+      </div>
+      <h2 className="so-inverse" id="navbar_head">
+        SO INVERSE
+      </h2>
+    </div>
+  );
+};
+
+export default MovieScreen;
